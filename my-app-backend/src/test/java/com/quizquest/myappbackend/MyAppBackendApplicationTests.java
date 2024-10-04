531c9af2f7e3ab749f415cbbdd7e6fdc8141fed9
phamdatzx <megumikatoubuhbuh@gmail.com>
2024-10-04 22:25:24 +0700

    initialize project

---
+++ my-app-backend/src/test/java/com/quizquest/myappbackend/MyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizquest.myappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
